ed9eece680d1d582399b5bb4f44a8f2b3662ec2b
Sn0wFox <ruben.pericas@laposte.net>
2016-11-27 15:49:41 +0100

    Fully fonctionnal angular app

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -3,7 +3,10 @@ import {Component} from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'othello-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent {
   protected name: string = "Othello App";
