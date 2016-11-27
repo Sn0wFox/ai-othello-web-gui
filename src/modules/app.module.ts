541fdd270fae2693420fa42d3c0905fd5c8112ce
Sn0wFox <ruben.pericas@laposte.net>
2016-11-27 18:48:02 +0100

    Added PrologService

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 
-import { AppComponent }  from '../components/app/app.component';
-import { BoardComponent }  from '../components/board/board.component';
+import { AppComponent }   from '../components/app/app.component';
+import { BoardComponent } from '../components/board/board.component';
+
+import { PrologService }  from '../services/prolog/prolog.service';
+
+
 
 @NgModule({
   imports: [
@@ -15,6 +19,9 @@ import { BoardComponent }  from '../components/board/board.component';
   declarations: [
     AppComponent,
     BoardComponent
+  ],
+  providers: [
+    PrologService
   ],
   bootstrap: [ AppComponent ]
 })
