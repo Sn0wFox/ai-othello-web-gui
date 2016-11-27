44ec99e479ce0691222668803d025da38acc8733
Sn0wFox <ruben.pericas@laposte.net>
2016-11-27 17:46:53 +0100

    Preparing board...

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from '../components/app/app.component';
 import { BoardComponent }  from '../components/board/board.component';
+import { TokenComponent }  from '../components/token/token.component';
 
 @NgModule({
   imports: [
