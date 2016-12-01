a1da203bcbdc4833a4de04b970c8e88930f18fea
Sn0wFox <ruben.pericas@laposte.net>
2016-12-01 22:19:24 +0100

    Added api overlayer for prolog server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as path from 'path';
+import * as bodyParser from 'body-parser';
+import * as request from 'request';
 
 /**
  * Define parameters
@@ -8,12 +10,135 @@ import * as path from 'path';
 const ROOT = path.resolve(__dirname, '../');  // The root directory
 const HOST = 'localhost';                     // The host where the server will run
 const PORT = 3000;                            // The port on which the server will run
+const PRO_HOST = 'localhost';                 // The host where the prolog server is running
+const PRO_PORT = 8000;                        // The post on which the prolog server is running
+const PRO_PROTOCOL = 'http://';               // The protocol used by the prolog server
 
 /**
  * Create the server
  */
 let app = express();
 app.use(express.static(ROOT));    // Basic files distribution: everything asked from root
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * API overlayer for the Prolog's server API
+ */
+
+/*
+ * /api/board/initial
+ */
+app.get("/api/board/initial", (req, res, next) => {
+  request.post(
+    {
+      url: PRO_PROTOCOL + PRO_HOST + ':' + PRO_PORT + "/api/board/initial"
+    },
+    (err, httpResponse, body): void => {
+        if (err) {
+          res.status(500).end();
+          return;
+        }
+        console.log(body);
+        res.status(200).send(body);
+      }
+    )
+});
+
+/*
+ * /api/board/update
+ */
+app.post("/api/board/update", (req, res, next) => {
+  console.log("Body:" + req.body);
+  let params: string =
+    "?player=" + req.query["player"]
+    + "&movex=" + req.query["movex"]
+    + "&movey=" + req.query["movey"];
+  request.post(
+    {
+      url: PRO_PROTOCOL + PRO_HOST + ':' + PRO_PORT + "/api/board/update" + params,
+      json: true,
+      body: req.body
+    },
+    (err, httpResponse, body): void => {
+        if (err) {
+          res.status(500).end();
+          return;
+        }
+        res.status(200).send(body);
+      }
+    )
+});
+
+/*
+ * /api/play
+ */
+app.post("/api/play", (req, res, next) => {
+  let params: string =
+    "?player=" + req.query["player"]
+    + "&ai=" + req.query["ai"];
+  request.post(
+    {
+      url: PRO_PROTOCOL + PRO_HOST + ':' + PRO_PORT + "/api/play" + params,
+      json: true,
+      body: req.body
+    },
+    (err, httpResponse, body): void => {
+        if (err) {
+          res.status(500).end();
+          return;
+        }
+        res.status(200).send(body);
+      }
+    )
+});
+
+/*
+ * /api/play/validate
+ */
+app.post("/api/play/validate", (req, res, next) => {
+  let params: string =
+    "?player=" + req.query["player"]
+    + "&movex=" + req.query["movex"]
+    + "&movey=" + req.query["movey"];
+  request.post(
+    {
+      url: PRO_PROTOCOL + PRO_HOST + ':' + PRO_PORT + "/api/play/validate" + params,
+      json: true,
+      body: req.body
+    },
+    (err, httpResponse, body): void => {
+        if (err) {
+          res.status(500).end();
+          return;
+        }
+        res.status(200).send(body);
+      }
+    )
+});
+
+/*
+ * /api/play/able
+ */
+app.post("/api/play/able", (req, res, next) => {
+  let params: string =
+    "?player=" + req.query["player"];
+  request.post(
+    {
+      url: PRO_PROTOCOL + PRO_HOST + ':' + PRO_PORT + "/api/play/able" + params,
+      json: true,
+      body: req.body
+    },
+    (err, httpResponse, body): void => {
+        if (err) {
+          res.status(500).end();
+          return;
+        }
+        res.status(200).send(body);
+      }
+    )
+});
+
 
 // Actually instanciate the server
 let server = http.createServer(app);
